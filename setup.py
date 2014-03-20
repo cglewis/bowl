3a874ef17a88e1f4805f6e00654619be7b398154
cglewis <charliel@lab41.org>
2014-03-19 22:56:39 -0700

    add dockerfiles in package, bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='bowl',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     license='LICENSE',
     description='Tool for easily building and configuring virtual environments on top of Docker.',
     long_description=open('README.md').read(),
     author=u'Charlie Lewis',
     author_email='charliel@lab41.org',
+    package_data={'': ['LICENSE'], 'bowl': ['containers/*/*/databases/dockerfiles/*/Dockerfile', 'containers/*/*/environment/dockerfiles/*/Dockerfile', 'containers/*/*/services/dockerfiles/*/Dockerfile']},
     entry_points={
         'console_scripts': [
             'bowl = bowl.cli:main',
