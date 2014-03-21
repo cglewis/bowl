8d181a343057fd8bae61590a0d157dd5ef3a3132
cglewis <charliel@lab41.org>
2014-03-20 17:26:30 -0700

    use personal email address

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Tool for easily building and configuring virtual environments on top of Docker.',
     long_description=open('README.md').read(),
     author=u'Charlie Lewis',
-    author_email='charliel@lab41.org',
+    author_email='charlie.lewis42@gmail.com',
     package_data={'': ['LICENSE'], 'bowl': ['containers/*/*/databases/dockerfiles/*/Dockerfile', 'containers/*/*/environment/dockerfiles/*/Dockerfile', 'containers/*/*/services/dockerfiles/*/Dockerfile']},
     entry_points={
         'console_scripts': [
