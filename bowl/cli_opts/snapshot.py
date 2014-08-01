4a680f022be87a3b133aff25f4d943c4e0abde8d
cglewis <clewis@iqt.org>
2014-07-31 20:59:16 -0700

    working snapshotting

--- bowl/cli_opts/snapshot.py
+++ bowl/cli_opts/snapshot.py
@@ -6,6 +6,7 @@ Created on 15 March 2014
 """
 import docker
 import os
+import uuid
 
 from bowl.cli_opts import list
 
@@ -28,21 +29,40 @@ class snapshot(object):
         list_args.z = True
         list_a = list.list.main(list_args)
 
+        commit = str(uuid.uuid4())
+        snapshot = {}
         found = 0
         for container in list_a:
             if args.CONTAINER in container:
-                # !! TODO
-                print list_a
-                found = 1
+                 host = container.split(",")[1]
+                 cont = container.split(",")[0]
+                 commit_name = "bowl-snapshot-"+commit
+                 try:
+                     c = docker.Client(base_url='tcp://'+host+':2375', version='1.9',
+                                       timeout=2)
+                     c.commit(cont, repository=commit_name)
+                     snapshot[cont] = host+":"+commit_name
+                 except:
+                     if not args.z:
+                         print "unable to connect to "+host
+                     return False
+                 found = 1
 
         if found:
-            snapshot_id = []
             try:
                 with open(os.path.join(directory, "snapshots"), 'a') as f:
-                    # !! TODO
-                    f.write("{" +
-                            "'title': '"+
-                            "}\n")
+                    for container in snapshot:
+                        f.write("{" +
+                                "'container_id': '"+container+"'," +
+                                " 'snapshot_id': '"+snapshot[container].split(":")[1]+"'," +
+                                " 'host': '"+snapshot[container].split(":")[0]+"'" +
+                                "}\n")
+                        if not args.z:
+                            print "snapshotted",
+                            print container + " on",
+                            print snapshot[container].split(":")[0],
+                            print "to repository;",
+                            print snapshot[container].split(":")[1]
             except:
                 if not args.z:
                     print "unable to snapshot container"
