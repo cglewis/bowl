775727c4ca972b26daf5060ebaed940c30175e98
cglewis <clewis@iqt.org>
2014-08-03 19:48:33 -0700

    fix test for docker host not running

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,7 @@ Created on 27 July 2014
 import pytest
 import sys
 
-from docker import client
+from requests import exceptions
 from bowl import cli
 
 class TestClass:
@@ -21,6 +21,6 @@ class TestClass:
             cli.main()
         sys.argv = ["", "new", "-n"]
         cli.main()
-        sys.argv = ["", "new", "-n", "--host", "localhost", "-s", "os:version:type:service", "-l"]
-        with pytest.raises(client.APIError):
+        sys.argv = ["", "new", "-n", "--host", "test", "-s", "os:version:type:service", "-l"]
+        with pytest.raises(exceptions.ConnectionError):
             cli.main()
