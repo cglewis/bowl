109062925a1cc3c83f55c7fe77383973abf931f3
cglewis <clewis@iqt.org>
2014-08-10 15:18:33 -0400

    more tests for new

--- tests/cli_opts/test_cli_opts_new.py
+++ tests/cli_opts/test_cli_opts_new.py
@@ -4,8 +4,10 @@ This module is the test suite of the new CLI option for bowl.
 Created on 27 July 2014
 @author: Charlie Lewis
 """
+import pytest
 
 from bowl.cli_opts import new
+from requests import exceptions
 
 class Object(object):
     pass
@@ -20,3 +22,39 @@ class TestClass:
         a = new.new()
         menu_dict = a.build_options(self, args)
         assert isinstance(menu_dict, dict)
+
+        args.no_curses = True
+        args.host = []
+        args.host.append("test")
+
+        args.service = []
+        args.service.append("os:version:type:service")
+        args.image = False
+
+        args.command = False
+        args.entrypoint = False
+        args.volume = False
+        args.port = False
+        args.link = False
+        args.name = False
+        args.unique = False
+        args.user = False
+
+        with pytest.raises(exceptions.ConnectionError):
+            a.main(args)
+
+        #args.command = True
+        #args.entrypoint = True
+        #args.volume = True
+        #args.port = True
+        #args.link = True
+        #args.name = True
+        #args.unique = True
+        #args.user = True
+
+        #a.main(args)
+
+        #args.service = False
+        #args.image = True
+
+        #a.main(args)
