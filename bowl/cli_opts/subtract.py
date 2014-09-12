fdbc2d86ee8f9d9e93aeb16ab7f3901f42b853d9
cglewis <clewis@iqt.org>
2014-09-11 20:08:16 -0700

    incomplete subtract, but a start

--- bowl/cli_opts/subtract.py
+++ bowl/cli_opts/subtract.py
@@ -4,6 +4,9 @@ This module is the subtract command of bowl.
 Created on 1 September 2014
 @author: Charlie Lewis
 """
+import shutil
+import os
+import sys
 
 class subtract(object):
     """
@@ -11,5 +14,30 @@ class subtract(object):
     """
     @classmethod
     def main(self, args):
-        # !! TODO
-        print args
+        if os.path.exists(os.path.join(args.metadata_path,
+                                       args.OS,
+                                       args.VERSION,
+                                       args.TYPE,
+                                       'dockerfiles',
+                                       args.NAME)):
+            # remove dockerfiles
+            shutil.rmtree(os.path.join(args.metadata_path,
+                                       args.OS,
+                                       args.VERSION,
+                                       args.TYPE,
+                                       'dockerfiles',
+                                       args.NAME))
+            # !! TODO
+            # remove service
+            # check if last item (need to remove comma from previous service if there is one
+            # check if not last item '},' as opposed to '}'
+            # if result is just '{}' then do following todos, otherwise skip"
+
+            # !! TODO
+            # if version services are empty, remove it
+
+            # !! TODO
+            # if os versions are empty, remove it
+        else:
+            print "Service doesn't exist."
+            sys.exit(0)
