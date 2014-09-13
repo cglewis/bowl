2c132e4e694db729319d69804f9031f6d2c9e43c
cglewis <clewis@iqt.org>
2014-09-13 15:18:52 -0700

    finish subtract

--- bowl/cli_opts/subtract.py
+++ bowl/cli_opts/subtract.py
@@ -14,7 +14,9 @@ class subtract(object):
     """
     @classmethod
     def main(self, args):
+        args.metadata_path = os.path.expanduser(args.metadata_path)
         if os.path.exists(os.path.join(args.metadata_path,
+                                       'services',
                                        args.OS,
                                        args.VERSION,
                                        args.TYPE,
@@ -22,6 +24,7 @@ class subtract(object):
                                        args.NAME)):
             # remove dockerfiles
             shutil.rmtree(os.path.join(args.metadata_path,
+                                       'services',
                                        args.OS,
                                        args.VERSION,
                                        args.TYPE,
@@ -29,22 +32,100 @@ class subtract(object):
                                        args.NAME))
             # remove service
             found = 0
-            for line in fileinput.input(os.path.join(args.metadata_path, args.OS, args.VERSION, args.TYPE, args.TYPE), inplace=True):
+            for line in fileinput.input(os.path.join(args.metadata_path, 'services', args.OS, args.VERSION, args.TYPE, args.TYPE), inplace=True):
                 if args.NAME in line:
                     found = 1
                 elif found == 1:
                     if line == " }\n" or line == " },\n":
                         found = 0
                 else:
-                    print "%s" (line),
+                    print "%s" % (line),
 
-            # !! TODO
-            # if result is just '{}' then do following todos, otherwise skip"
+            empty = 1
+            with open(os.path.join(args.metadata_path, 'services', args.OS, args.VERSION, 'databases/databases'), 'r') as f:
+                fl = f.readline()
+                if fl != "{\n":
+                    empty = 0
+                else:
+                    sl = f.readline()
+                    if sl != "}\n":
+                        emtpy = 0
+            with open(os.path.join(args.metadata_path, 'services', args.OS, args.VERSION, 'environment/environment'), 'r') as f:
+                fl = f.readline()
+                if fl != "{\n":
+                    empty = 0
+                else:
+                    sl = f.readline()
+                    if sl != "}\n":
+                        emtpy = 0
+            with open(os.path.join(args.metadata_path, 'services', args.OS, args.VERSION, 'services/services'), 'r') as f:
+                fl = f.readline()
+                if fl != "{\n":
+                    empty = 0
+                else:
+                    sl = f.readline()
+                    if sl != "}\n":
+                        emtpy = 0
+            with open(os.path.join(args.metadata_path, 'services', args.OS, args.VERSION, 'tools/tools'), 'r') as f:
+                fl = f.readline()
+                if fl != "{\n":
+                    empty = 0
+                else:
+                    sl = f.readline()
+                    if sl != "}\n":
+                        emtpy = 0
 
-            # !! TODO
-            # if version services are empty, remove it and dockerfiles dir
+            if empty == 1:
+                # if version services are empty, remove it and dockerfiles dir
+                shutil.rmtree(os.path.join(args.metadata_path, 'services', args.OS, args.VERSION))
 
-            # !! TODO
-            # if os versions are empty, remove it
+                # remove service
+                found = 0
+                for line in fileinput.input(os.path.join(args.metadata_path, 'services', args.OS, 'versions'), inplace=True):
+                    if args.VERSION in line:
+                        found = 1
+                    elif found == 1:
+                        if line == " }\n" or line == " },\n":
+                            found = 0
+                    else:
+                        print "%s" % (line),
+
+                empty = 1
+                with open(os.path.join(args.metadata_path, 'services', args.OS, 'versions'), 'r') as f:
+                    fl = f.readline()
+                    if fl != "{\n":
+                        empty = 0
+                    else:
+                        sl = f.readline()
+                        if sl != "}\n":
+                            emtpy = 0
+
+                if empty == 1:
+                    # if os versions are empty, remove it
+                    shutil.rmtree(os.path.join(args.metadata_path, 'services', args.OS))
+
+                    # remove service
+                    found = 0
+                    for line in fileinput.input(os.path.join(args.metadata_path, 'services', 'oses'), inplace=True):
+                        if args.OS in line:
+                            found = 1
+                        elif found == 1:
+                            if line == " }\n" or line == " },\n":
+                                found = 0
+                        else:
+                            print "%s" % (line),
+
+                    empty = 1
+                    with open(os.path.join(args.metadata_path, 'services', 'oses'), 'r') as f:
+                        fl = f.readline()
+                        if fl != "{\n":
+                            empty = 0
+                        else:
+                            sl = f.readline()
+                            if sl != "}\n":
+                                emtpy = 0
+                    if empty == 1:
+                        # if oses are empty, remove services
+                        shutil.rmtree(os.path.join(args.metadata_path, 'services'))
         else:
             print "Service doesn't exist."
