252dee5bf48e0d6ea24da13e6a67b2ff846886be
Charlie Lewis <clewis@iqt.org>
2014-12-14 22:33:30 -0500

    write out extra options for connected hosts

--- bowl/cli_opts/connect.py
+++ bowl/cli_opts/connect.py
@@ -37,6 +37,18 @@ class connect(object):
                     host_dict['title'] = args.DOCKER_HOST
                     host_dict['port'] = args.port
                     host_dict['type'] = 'choice_menu'
+                    if args.e:
+                        host_dict['env'] = 'true'
+                    if args.sock:
+                        host_dict['sock'] = args.sock
+                    if args.tlsverify:
+                        host_dict['tlsverify'] = 'true'
+                    if args.tlscert:
+                        host_dict['tlscert'] = args.tlscert
+                    if args.tlskey:
+                        host_dict['tlskey'] = args.tlskey
+                    if args.tlscacert:
+                        host_dict['tlscacert'] = args.tlscacert
                     f.write(str(host_dict)+"\n")
             else:
                 print "host has already been connected"
